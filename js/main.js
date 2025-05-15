242dad7af14fca2613525f441349a3d756942b55
ziad9899 <alofi9899@gmail.com>
2025-05-15 13:43:34 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
-// ملف main.js
-
 function toggleSidebar() {
   const sidebar = document.getElementById("sidebar");
   sidebar.classList.toggle("active");
 }
 
-// إغلاق القائمة عند الضغط على رابط داخلها (اختياري لتحسين UX)
 document.addEventListener("DOMContentLoaded", function () {
   const sidebarLinks = document.querySelectorAll(".sidebar a");
   sidebarLinks.forEach(link => {
