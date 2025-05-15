a465d07ffe556cd23ee3930238c3223f8bd1f25e
ziad9899 <alofi9899@gmail.com>
2025-05-15 06:51:19 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -46,3 +46,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("active");
+}
